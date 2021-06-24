72c1b3a5030d9e599f79cf8b0d478eec1c2ffb0a
savchuckvadim <savchuckvadim@gmail.com>
2021-06-24 16:55:00 +0300

    cats commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//Получение ответа на запрос Fetch через асинхронную функцию и await перед fetch
+let api = 'http://jsonplaceholder.typicode.com/photos'
+let apicats = 'https://api.thecatapi.com/v1/images/search'
+
+
+async function getResponse() {
+    let response = await fetch(apicats);
+    let json = await response.json();
+    let content = json.splice(0, 10); // оставляем только 10 элементов из всего массива json
+    let list = document.querySelector('.post');
+
+     console.log(json);
+
+    // for(let i=0; i< 10; i++){
+    //     console.log(json[i].url);
+    // }
+
+    let key;
+    for (key in content) {
+        console.log(content[key].url);
+
+        list.innerHTML += `
+        <ul>
+            <li>
+                
+                <img src="${content[key].url}" id="cats" alt="img${key}">
+            </li>
+       
+        </ul>
+    `
+    }
+    //await setTimeout(document.location.reload(), 10000);
+}
+
+
+getResponse();
+
+
+
+// let htmlCats = document.querySelector('#cats');
+
+// document.addEventListener('click', ()=>{
+//   document.location.reload();
+// })
+
+
+
+
+
